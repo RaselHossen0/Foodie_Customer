d35728c92e7a0bf0eeacb37075c4eb5b000106cb
Md. Rasel Hossen <hrasel2002@gmail.com>
2024-04-25 22:53:19 +0600

    hjk

--- lib/ui/home/HomeScreen.dart
+++ lib/ui/home/HomeScreen.dart
@@ -6,15 +6,12 @@ import 'package:clipboard/clipboard.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart' as loc;
 import 'package:location/location.dart';
-import 'package:place_picker/place_picker.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:story_view/story_view.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:place_picker/entities/location_result.dart';
+import 'package:place_picker/widgets/place_picker.dart';
 
 import '../../AppGlobal.dart';
 import '../../constants.dart';
